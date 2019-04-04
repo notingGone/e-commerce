307794fa305aecb23264c303677f350394e98042
Elliott C. Woodward <elliottcwoodward@gmail.com>
2019-04-04 17:02:47 -0400

    Add destroy product button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role, name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:role, name])
   end
 
   def current_order
