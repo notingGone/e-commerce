4779886e17279c0d3d4630b80acb3f038b4b72b7
Elliott C. Woodward <elliottcwoodward@gmail.com>
2019-04-06 17:22:28 -0400

    adding the same item twice no longer creates another line item, instead, increases quantity in existant line item.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,16 +1,27 @@
 class CartController < ApplicationController
   before_action :authenticate_user!, except: [:add_to_cart, :view_order, :clear_cart, :delete_item]
+  before_action :set_line_item, only: :delete_item
 
   def add_to_cart
     @order = current_order
-    @order.save
+    @order.save if @order.id.nil?
     session[:order_id] = @order.id
-    # line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
-    # line_item = LineItem.find(params[:line_item_id])
-    # line_item.update(line_item_params)
-    line_item = LineItem.create(line_item_params)
-    line_item.update(order_id: @order.id, line_item_total: (line_item.quantity * line_item.product.price))
-    # line_item.update(line_item_total: (line_item.quantity * line_item.product.price))
+
+# debugger
+    # if @order.line_items.pluck(:product_id).uniq.include? line_item_params['product_id'].to_i
+    #   line_item = LineItem.find_by(product_id: line_item_params['product_id'].to_i)
+    #   line_item.update(quantity: line_item.quantity + line_item_params['quantity'].to_i)
+    # else
+    #   line_item = LineItem.create(line_item_params)
+    # end
+
+    line_item = @order.line_items.find_or_create_by(product_id: line_item_params['product_id'].to_i)
+    line_item.quantity.nil? ? line_item.update(quantity: line_item_params['quantity'].to_i) :
+                              line_item.update(quantity: line_item.quantity +
+                              line_item_params['quantity'].to_i)
+    line_item.update(order_id: @order.id)
+    line_item.update(line_item_total: (line_item.quantity * line_item.product.price))
+    # line_item.save
     redirect_back(fallback_location: root_path)
   end
 
@@ -25,7 +36,6 @@ class CartController < ApplicationController
 
   def delete_item
     current_order.line_items.find_by(product_id: params[:product_id]).delete
-    # redirect_back(fallback_location: root_path)
     view_order
     set_cart_count
     render :view_order
@@ -35,12 +45,14 @@ class CartController < ApplicationController
     @order = current_order
 
     if(@order.line_items.length > 0)
+
       @order.subtotal = 0
       @order.line_items.each do |line_item|
         line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
         @order.order_items[line_item.product_id] = line_item.quantity
         @order.subtotal += line_item.line_item_total
       end
+
       @order.save
       @order.update(sales_tax: (@order.subtotal * 0.07))
       @order.update(grand_total: (@order.sales_tax + @order.subtotal))
@@ -54,8 +66,7 @@ class CartController < ApplicationController
   private
 
     def line_item_params
-      # debugger
-      params.require(:line_item).permit(:product_id, :quantity, :order_id)
+      params.require(:line_item).permit(:product_id, :quantity, :order_id, :line_item_id)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'categorical' => 'storefront#items_by_category'
   get 'branding' => 'storefront#items_by_brand'
   get 'clear_cart' => 'cart#clear_cart'
-  get 'delete_item' => 'cart#delete_item'
+  post 'delete_item' => 'cart#delete_item'
   devise_for :users
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
