2be71f7b817243d0f9c17c99c8c0ab6365013654
Elliott C. Woodward <elliottcwoodward@gmail.com>
2019-04-06 11:15:04 -0400

    Hover styles! Quantity validation.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :order, optional: true
+  validates :quantity, presence: true
+  validates :quantity, numericality:
+    { less_than_or_equal_to: proc { |m| m.product.quantity },
+      greater_than: 0 }
 end
